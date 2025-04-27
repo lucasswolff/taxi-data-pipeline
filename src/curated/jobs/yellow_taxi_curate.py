4e1851d890ce67b6da3ff6b664f20c33d8872702
Lucas Wolff <lucaswolff494@gmail.com>
2025-04-27 14:08:43 -0300

    organized src curated directory structure

--- src/curated/jobs/yellow_taxi_curate.py
+++ src/curated/jobs/yellow_taxi_curate.py
@@ -1,7 +1,10 @@
-from create_spark import create_spark_session
-from taxi_transformer import TransformerBase, TransformerYellowGreen
-from read_lockup_tables import ReadLockup
-from transform_run_mode import TransformMode
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+from curated.utils.create_spark import create_spark_session
+from curated.transformations.taxi_transformer import TransformerBase, TransformerYellowGreen
+from curated.utils.read_lockup_tables import ReadLockup
+from curated.transformations.transform_run_mode import TransformMode
 
 def main(run_mode, months, trans_mode, raw_folder_path, lockup_folder_path, curated_folder_path):
     #### create spark session
@@ -10,30 +13,31 @@ def main(run_mode, months, trans_mode, raw_folder_path, lockup_folder_path, cura
     #### read parquet files
     print('Reading files...')
     
-    taxi = 'green'
+    taxi = 'yellow'
     file_path = trans_mode.get_run_mode_files(run_mode, months, raw_folder_path, taxi) # get file path based on run parameters
-    df_green_raw = spark.read.parquet(*file_path) 
+    df_yellow_raw = spark.read.parquet(*file_path) 
     
     lockup_reader = ReadLockup()
     df_payment_type, df_ratecode, df_trip_type, df_taxi_zone = lockup_reader.read_lockup_tables(spark, lockup_folder_path)
     
+    
     #### transform df
     print('Initiating dataframe transformation...')
     
-    transformer_green = TransformerYellowGreen()
-    df_green = transformer_green.make_green_consistent(df_green_raw) # add columns with defaut values to be consistent with yellow taxi
+    transformer_yellow = TransformerYellowGreen()
+    df_yellow = transformer_yellow.make_yellow_consistent(df_yellow_raw) # add columns with defaut values to be consistent with green taxi
     
     transformer = TransformerBase()
-    df_green = transformer.transform_df(df_green, df_payment_type, df_ratecode, df_trip_type, df_taxi_zone) 
+    df_yellow = transformer.transform_df(df_yellow, df_payment_type, df_ratecode, df_trip_type, df_taxi_zone) 
     
     
     #### upload into curated layer
     print('Saving files...')
     
-    # use df_green.coalesce(2).write when in AWS
+    # use df_yellow.coalesce(2).write when in AWS
     
     if run_mode == 'full_load':
-        df_green.coalesce(2).write \
+        df_yellow.coalesce(2).write \
             .partitionBy('file_year', 'file_month') \
             .mode('overwrite') \
             .parquet(curated_folder_path)
@@ -41,7 +45,7 @@ def main(run_mode, months, trans_mode, raw_folder_path, lockup_folder_path, cura
     else: 
         trans_mode.delete_parquet_folders(run_mode, months, curated_folder_path, raw_folder_path)
         
-        df_green.coalesce(2).write \
+        df_yellow.coalesce(2).write \
             .partitionBy('file_year', 'file_month') \
             .mode('append') \
             .parquet(curated_folder_path)
@@ -55,9 +59,9 @@ if __name__ == '__main__':
     run_mode, months = trans_mode.parse_args()
     
     ### change it when in AWS
-    raw_folder_path = "sample_data/raw/green/"
+    raw_folder_path = "sample_data/raw/yellow/"
     lockup_folder_path = "lockup_tables/"
-    curated_folder_path = "sample_data/curated/green"
+    curated_folder_path = "sample_data/curated/yellow"
 
     main(run_mode, months, trans_mode, raw_folder_path, lockup_folder_path, curated_folder_path)
 
