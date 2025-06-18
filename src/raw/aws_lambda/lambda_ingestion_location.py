b56a55294f9390ba9e14f991e8f2e54728c6b5e8
lucasswolff <lucas@DESKTOP-EP7NDA3.localdomain>
2025-06-18 11:14:57 -0300

    create lambda raw ingestion job

---
+++ src/raw/aws_lambda/lambda_ingestion_location.py
@@ -0,0 +1,28 @@
+import requests
+import boto3
+import os
+from download_upload_s3 import download_and_upload_to_s3
+
+s3 = boto3.client('s3')
+bucket_name = 'taxi-data-hub'  
+
+def main():
+   
+    url = 'https://d37ci6vzurychx.cloudfront.net/misc/taxi_zone_lookup.csv'
+    base_s3_prefix = 'lockup_tables/' 
+    file_name = 'taxi_zone.csv'
+    s3_key = f'{base_s3_prefix}{file_name}'
+
+    print(f'Downloading location file')
+
+    # skip upload if file exists in S3
+    try:
+        s3.head_object(Bucket=bucket_name, Key=s3_key)
+        print(f'{s3_key} already in S3, skipping. \n')
+    except s3.exceptions.ClientError:
+        print(f'Downloading and uploading {s3_key}')
+        download_and_upload_to_s3(url, s3, bucket_name, s3_key)
+        
+               
+if __name__ == '__main__':
+    main()
