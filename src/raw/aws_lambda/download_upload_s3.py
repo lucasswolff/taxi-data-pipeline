3a143774e707bec3545acd72e64eb2cce9e1c42b
Lucas Wolff <lucaswolff494@gmail.com>
2025-06-23 12:02:12 -0300

    update upload to s3 function

--- src/raw/aws_lambda/download_upload_s3.py
+++ src/raw/aws_lambda/download_upload_s3.py
@@ -3,10 +3,10 @@ import boto3
 import os
 
 def download_and_upload_to_s3(url, s3, bucket_name, s3_key):
-    response = requests.get(url)
+    response = requests.get(url, stream=True)
 
     if response.status_code == 200:
-        s3.put_object(Bucket=bucket_name, Key=s3_key, Body=response.content)
+        s3.upload_fileobj(response.raw, Bucket=bucket_name, Key=s3_key)
         print(f'Uploaded to s3://{bucket_name}/{s3_key} \n')
     elif response.status_code == 403:
         print(f'Failed to download {url}. Error code: {response.status_code}. The file might not exist. \n')
